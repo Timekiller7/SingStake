1881987f287f795258abe3efbcb09eb363b7329f
Nastya <nastisha.m@gmail.com>
2022-11-27 20:14:34 +0300

    example fix

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,30 +1,29 @@
 module Eval (evalT, evalWithArgsT) where
 
+import Data.Bifunctor (first)
 import Data.Default (def)
 import Data.Text (Text, pack)
-import Data.Bifunctor (first)
-import qualified Data.Text.Encoding as TE
+import Data.Text.Encoding qualified as TE
 import Plutarch (ClosedTerm, compile)
 import Plutarch.Evaluate (evalScript)
+import Plutarch.Script (Script (..))
 import PlutusLedgerApi.V1 (Data, ExBudget)
-import Plutarch.Script (Script(unScript), Script(..))
 import UntypedPlutusCore (DeBruijn, DefaultFun, DefaultUni, Program)
 
-import UntypedPlutusCore qualified as UPLC
-import UntypedPlutusCore.Check.Scope qualified as UPLC
-import UntypedPlutusCore.Evaluation.Machine.Cek qualified as UPLC
 import PlutusCore qualified as PLC
 import PlutusCore.Data qualified as PLC
 import PlutusCore.Evaluation.Machine.ExBudget qualified as PLC
+import UntypedPlutusCore qualified as UPLC
+import UntypedPlutusCore.Check.Scope qualified as UPLC
+import UntypedPlutusCore.Evaluation.Machine.Cek qualified as UPLC
 
 import PlutusCore.MkPlc qualified as PLC
 
-
 applyArguments :: Script -> [Data] -> Script
 applyArguments (Script (UPLC.Program a v t)) args =
-    let termArgs = fmap (PLC.mkConstant ()) args
-        applied = PLC.mkIterApp () t termArgs
-    in Script (UPLC.Program a v applied)
+  let termArgs = fmap (PLC.mkConstant ()) args
+      applied = PLC.mkIterApp () t termArgs
+   in Script (UPLC.Program a v applied)
 
 evalT :: ClosedTerm a -> Either Text (Script, ExBudget, [Text])
 evalT x = evalWithArgsT x []
@@ -40,4 +39,3 @@ evalWithArgsT' :: ClosedTerm a -> [Data] -> Either Text (Program DeBruijn Defaul
 evalWithArgsT' x args =
   (\(res, budg, trcs) -> (unScript res, budg, trcs))
     <$> evalWithArgsT x args
-  
--- src/Example.hs
+++ src/Example.hs
@@ -1,9 +1,9 @@
 module Example (exampleFunction) where
 
+import Eval
 import Plutarch.Integer (PInteger)
 import Plutarch.Monadic qualified as P
 import Plutarch.Prelude
-import Eval
 
 exampleFunction :: Term s PInteger
 exampleFunction = 1 + 3
